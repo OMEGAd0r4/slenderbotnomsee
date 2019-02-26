f2bca06a20ffb06bdaf68e36abf28ca5dc50fed5
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2019-02-26 19:41:51 +0700

    Create botreload.js

---
+++ commands/admin/botreload.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js");
+const commando = require('discord.js-commando');
+const prefix = "!";
+const bot = new commando.Client({
+  commandPrefix: prefix
+});
+
+class botreloadCommand extends commando.Command {
+  constructor(client) 
+  {
+    super(client, {
+      name: 'botreload', 
+      group: 'admin',
+      memberName: 'botreload',
+      description: "Reloads the slender bot"
+    });
+  }
+
+  async run (message, args)
+  {
+
+    message.delete(3000).catch(O_o=>{});
+
+    var botownerrole = message.guild.roles.find(`name`, "*");
+
+    if (!message.member.roles.has(botownerrole.id)) return message.channel.send("Insufficient permission. You do not have permission to reload the bot");
+
+    if (message.member.roles.has(botownerrole.id))
+
+    {
+
+      bot.destroy()
+
+      bot.login("NTQ3MzYwMzgyMjk0MTYzNDU4.D01ovA.B3atJErlNcIXVnRmx_aUHSPEw5Q");
+
+      console.clear()
+
+      message.channel.send("``Bot has been successfully reloaded``")
+    }
+
+
+  }
+}
+
+module.exports = botreloadCommand;
+
+//!announce <message>
