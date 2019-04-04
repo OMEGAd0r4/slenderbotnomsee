d531a5fb20bdb0acc3519852713345511dad2217
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2019-04-04 16:55:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ bot.on('message', async message => {
 //MESSAGE LOGS
 bot.on('message', async message => {
     //blacklisted words
-    let messagelogsmessage = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8". "9", "0" , "-" , "+" , "!", "2", ";", ":", "'", ".", "/", "?", "@", "#", "$", "%", "^", "&", "*", "(", ")", "`", "~", "<", ",", "[", "]", "{", "}"]; //words put , after the word
+    let messagelogsmessage = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" , "-" , "+" , "!", "2", ";", ":", "'", ".", "/", "?", "@", "#", "$", "%", "^", "&", "*", "(", ")", "`", "~", "<", ",", "[", "]", "{", "}"]; //words put , after the word
   
     //2 looking for words
     let foundInText = false;
