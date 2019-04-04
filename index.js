b4d7d8e5c2553017e2c33cd74b5dbc6da1a52234
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2019-04-04 17:15:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ bot.on('message', async message => {
 });
 
 bot.on("messageDelete", (messageDelete) => {
-        bot.channels.get('562278117717180426').send({embed: new Discord.RichEmbed()
+        bot.channels.get('563305590210691073').send({embed: new Discord.RichEmbed()
             .setTitle("**Slender [] Message Logs**")
             .setColor("#BFB4FF")
             .addField("Deleted message author", messageDelete.author.tag)
