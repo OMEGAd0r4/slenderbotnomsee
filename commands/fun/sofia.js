48ece1cfd33179bd22f913579787ebb818cd6d90
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2019-06-08 19:34:44 +0700

    Update sofia.js

--- commands/fun/sofia.js
+++ commands/fun/sofia.js
@@ -5,7 +5,7 @@ const bot = new commando.Client({
   commandPrefix: prefix
 });
 
-class bkeditCommand extends commando.Command {
+class sofiaCommand extends commando.Command {
   constructor(client) 
   {
     super(client, {
