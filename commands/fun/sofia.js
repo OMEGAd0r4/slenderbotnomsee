f00fd7413ef48ee1cb9c73666faf72c0f6ff34c9
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2019-06-08 19:43:04 +0700

    Update sofia.js

--- commands/fun/sofia.js
+++ commands/fun/sofia.js
@@ -20,15 +20,15 @@ class sofiaCommand extends commando.Command {
   {
 
     message.delete(3000).catch(O_o=>{});
+    
+        var sofiarole = message.guild.roles.find(`name`, "toxic tity");
+        
+    if (!message.member.roles.has(sofiarole.id)) return message.channel.send("``Insufficient permission. You do not have a role called 'toxic tity'``");
 
     message.channel.send({embed: new Discord.RichEmbed()
       .setTitle("**Sofia has autism not even joking**")
       .setColor("#f442df")
       .addField("STFU before I rip ur tiny 455 balls off.", "- Sofia 2k19")})
-
-    var sofiarole = message.guild.roles.find(`name`, "toxic tity");
-        
-    if (!message.member.roles.has(sofiarole.id)) return message.channel.send("``Insufficient permission. You do not have a role called 'toxic tity'``");
   }
 }
 
