edef47ec53fab4978071c80db6aa722effe812ab
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2020-03-21 12:08:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Music = require('discord.js-musicbot-addon');
 //PLUGINS
 
 //BOT TOKEN
-bot.login(process.env.token);
+bot.login("NjQ1NTU4MDc2MTE1NTE3NDYx.XnWhRQ.ryza1ZJRa3cFeOQbE0am8ZQhjAw");
 //BOT TOKEN
 
 //GETS THE BOT ONLINE
